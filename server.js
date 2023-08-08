ee74f4d458899001b14009e92252ab360d95383e
LanSanter <30773991+LanSanter@users.noreply.github.com>
2023-08-08 21:36:57 +0800

    Update server.js

--- server.js
+++ server.js
@@ -61,8 +61,9 @@ app.post('/openaimessage', async (req, res) => {
   while (param1.length > 20 || count_lens(param1) > 3500) {param1.splice(1,1)}; // 如果超過二十條訊息，刪除最舊的一條
   const result = await openai.openAiMessage(param1, 'gpt-3.5-turbo');
   console.log(result);
+  result = result + "@";
   // 將結果回傳給網頁端
-  res.json({ result + '@' });
+  res.json({ result});
 });
 
 app.listen(port, () => {
